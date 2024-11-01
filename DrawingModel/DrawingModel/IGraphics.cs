f0a58bdcc86f72f5dbe738c6a787fa6e4a4e6505
Yihss <a2685744@gmail.com>
2024-11-01 18:10:16 +0800

    ok

---
+++ DrawingModel/DrawingModel/IGraphics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingModel
+{
+    internal interface IGraphics
+    {
+        void ClearAll();
+        void DrawLine(double x1, double y1, double x2, double y2);
+        void DrawStart(double x1, double y1, double x2, double y2);
+        void DrawTerminator(double x1, double y1, double width, double height);
+        void DrawDecision(double x, double y, double width, double height);
+        void DrawProcess(double x, double y, double width, double height);
+        void DrawString(double x, double y, string shape_name); 
+    }
+}
